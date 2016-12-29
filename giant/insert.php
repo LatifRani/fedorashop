bdd7df38e6a88d0effe5e92170b18af6c1fcb536
LatifRani <latiparchina@gmail.com>
2016-12-30 02:06:33 +0800

    l
    a

---
+++ giant/insert.php
@@ -0,0 +1,29 @@
+<?php
+	include "connection.php";
+	
+	$product= $_POST['product'];
+	$price= $_POST['price'];
+	
+	class emp{}
+	
+	if (empty($product) || empty($price)) { 
+		$response = new emp();
+		$response->success = 0;
+		$response->message = "Required space must fill"; 
+		die(json_encode($response));
+	} else {
+		$query = mysql_query("INSERT INTO giant (id,product,price) VALUES(0,'".$product."','".$price."')");
+		
+		if ($query) {
+			$response = new emp();
+			$response->success = 1;
+			$response->message = "Data saved";
+			die(json_encode($response));
+		} else{ 
+			$response = new emp();
+			$response->success = 0;
+			$response->message = "Error data save";
+			die(json_encode($response)); 
+		}	
+	}
+?>
---
+++ giant/select.php
@@ -0,0 +1,16 @@
+<?php 
+	include "connection.php";
+	
+	$query = mysql_query("SELECT * FROM giant ORDER BY product ASC");
+	
+	$json = array();
+	
+	while($row = mysql_fetch_assoc($query)){
+		$json[] = $row;
+	}
+	
+	echo json_encode($json);
+	
+	mysql_close($connect);
+	
+?>
